ae231eb1ed63a797f872d32eb5b08b3fa08777d6
Yu <anneyu@6476baac6c20.ant.amazon.com>
2014-06-26 12:12:37 -0700

    Encode and decode string

---
+++ encode_decode.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string Decode(const string& s) {
+    string decoded;
+    if ( s.empty() ) {
+        return decoded;
+    }
+    
+    for ( int i = 0, n = 0; i < s.size(); i ++ ) {
+        if ( isalpha(s[i]) ) {
+            decoded.append(n, s[i]);
+            n = 0;    
+        } else if ( isdigit(s[i]) ) {
+            n = n * 10 + s[i] - '0';
+        }
+    }
+    return decoded;
+}
+
+int main() {
+    string org("2e3c11d0b");
+    cout << "org = " << org << endl;
+    cout << "decoded = " << Decode("2e3c11d0b") << endl;
+    return 0;
+}
